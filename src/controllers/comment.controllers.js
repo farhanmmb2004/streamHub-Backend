6fad0013e338135ec1fae44ae67b30d0771c2f99
farhan <farhankhanmmb2004@gmail.com>
2025-02-19 17:16:06 +0530

    completion of comment controller

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -1,14 +1,83 @@
-import mongoose from "mongoose"
+import mongoose,{isValidObjectId} from "mongoose"
 import {Comment} from "../models/comment.model.js"
+import { Vidio } from "../models/vidio.model.js"
+import { Like } from "../models/like.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
-    const {videoId} = req.params
+    const {vidioId} = req.params
     const {page = 1, limit = 10} = req.query
-
+    if(!isValidObjectId(vidioId)){
+    throw new ApiError(201,"invalid vidio id");
+    }
+    const vidio=await Vidio.findById(vidioId);
+    if(!vidio){
+    throw new ApiError(200,"vidio doesn't exists");
+    }
+    let comments=await Comment.aggregate([
+        {
+            $match:{
+                vidio:new mongoose.Types.ObjectId(vidioId)
+            }
+        },
+        {
+            $lookup:{
+                from:"users",
+                foreignField:"_id",
+                localField:"owner",
+                as:"ownerDetails",
+            }
+        },
+        {
+            $unwind:"$ownerDetails"
+        },
+        {
+            $lookup:{
+                from:"likes",
+                foreignField:"comment",
+                localField:"_id",
+                as:"likes",
+            }
+        },{
+            $addFields:{
+                likesCount:{$size:"$likes"},
+                isLiked:{
+                    $cond:{
+                        if:{
+                            $in:[req.user?._id,"$likes.likedBy"]
+                        },
+                       then:true,
+                       else:false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                _id:1,
+                content:1,
+                owner:1,
+                createdAt:1,
+                ownerDetails:{
+                    fullname:1,
+                    username:1,
+                    avtar:1,
+                },
+                likesCount:1,
+                isLiked:1
+            }
+        },{
+            $skip:(page-1)*limit
+        },{
+            $limit:parseInt(limit)
+        }
+    ]);
+    // let comments=await Comment.find({vidio:vidioId});
+    return res.status(200).json(new ApiResponse(200,comments,"aah aah chud gyi chud gyi"));
+    
 })
 
 const addComment = asyncHandler(async (req, res) => {
@@ -16,7 +85,7 @@ const addComment = asyncHandler(async (req, res) => {
     const {vidioId}=req.params;
     const{content}=req.body;
     if (!content) {
-        throw new apiError(400, "Content is required");
+        throw new ApiError(400, "Content is required");
     }
     const video = await Vidio.findById(vidioId);
 
@@ -37,12 +106,13 @@ if (!video) {
 const updateComment = asyncHandler(async (req, res) => {
     const{commentId}=req.params;
     const {newContent}=req.body;
+    if(!newContent){
+    throw new ApiError(400,"content is required")
+    }
     const comment=await Comment.findById(commentId);
     if(!comment){
         throw new ApiError(401,"comment doesn't exist");
     }
-    console.log(comment.owner);
-    console.log(req.user?._id);
     if(comment.owner.toString()!==req.user?._id.toString()){
     throw new ApiError(401,"only the owner of comment can edit this");
     }
@@ -68,6 +138,9 @@ const deleteComment = asyncHandler(async (req, res) => {
     }
     await Comment.deleteOne({
         _id:commentId
+    });
+    await Like.deleteMany({
+        comment:commentId
     })
     return res.status(200).json(new ApiResponse(200,{},"succesfully deleted"));
 })
