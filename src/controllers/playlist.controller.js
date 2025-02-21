2c6133c9be82e028cc1df90fb8ac325c7245fe24
farhan <farhankhanmmb2004@gmail.com>
2025-02-21 12:56:06 +0530

    completing playlist controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -22,11 +22,130 @@ const createPlaylist = asyncHandler(async (req, res) => {
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
     //TODO: get user playlists
+    let playlists=await Playlist.aggregate([
+        {
+            $match:{
+                owner:new mongoose.Types.ObjectId(userId)
+            }
+        },{
+            $lookup:{
+                from:"vidios",
+                localField:"vidios",
+                foreignField:"_id",
+                as:"vidioDetails"
+            }
+        },{
+            $addFields:{
+                totalVidios:{
+                    $size:"$vidioDetails"
+                },
+                veiws:{
+                    $sum:"$vidioDetails.veiws"
+                },
+                first:{
+                    $last:{
+                        $filter:{
+                            input:"$vidioDetails",
+                            as:"vidio",
+                            cond:{$eq:["$$vidio.isPublished",true]}
+                        }
+                    }
+                },
+            }
+        },{
+            $project:{
+               _id:1,
+               name:1,
+               description:1,
+               totalVidios:1,
+               veiws:1,
+               first:{
+                _id:1,
+                thumbnail:1,
+                views:1
+               },
+               updatedAt:1
+            }
+        }
+    ]);
+    return res.status(200).json(new ApiResponse(200,playlists,"success"));
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
-    //TODO: get playlist by id
+    if(!isValidObjectId(playlistId)){
+       throw new ApiError(400,"invalid vidio Id");
+    }
+    let playlist=await Playlist.aggregate([
+        {
+            $match:{
+            _id:new mongoose.Types.ObjectId(playlistId)
+            }
+        },{
+            $lookup:{
+                from:"vidios",
+                foreignField:"_id",
+                localField:"vidios",
+                as:"vidios"
+            }
+        },{
+            $set: {
+                vidios: {
+                    $filter: {
+                        input: "$vidios",
+                        as: "vidio",
+                        cond: { $eq: ["$$vidio.isPublished", true] }  
+                    }
+                }
+            }
+        },{
+            $lookup:{
+                from :"users",
+                foreignField:"_id",
+                localField:"owner",
+                as:"owner"
+            }
+        },{
+            $addFields:{
+                owner:{
+                    $first:"$owner"
+                },
+                totalVidios:{
+                    $size:"$vidios"
+                },
+                veiws:{
+                    $sum:"$vidios.veiws"
+                }
+            }
+        },{
+            $project:{
+                _id:1,
+                name:1,
+                description:1,
+                updatedAt:1,
+                createdAt:1,
+                vidios:{
+                    _id:1,
+                    title:1,
+                    description:1,
+                    views:1,
+                    duration:1,
+                    createdAt:1,
+                    thumbnail:1,
+                    vidioFile:1,
+                    isPublished:1
+                },
+                owner:{
+                    username:1,
+                    fullname:1,
+                    avtar:1
+                },
+                totalVidios:1,
+                veiws:1
+            }
+        }
+    ]);
+    return res.status(200).json(new ApiResponse(200,playlist,"success"));
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
