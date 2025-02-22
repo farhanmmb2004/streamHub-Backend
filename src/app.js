d993cae51a76ca8856253dd738802c76b8cd4835
farhan <farhankhanmmb2004@gmail.com>
2025-02-22 22:24:12 +0530

    fixing cors oringin

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ import cookieParser from "cookie-parser";
 import cors from 'cors';
 const app=express();
 app.use(cors({
-    origin:'*'
+    origin: process.env.CORS_ORIGIN,
+    Credentials: true,
 }));
 app.use(express.json({limit:"16Kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
