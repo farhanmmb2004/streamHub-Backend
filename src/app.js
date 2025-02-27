eb567485c970709c93573f2e1c49ba9cd85beec7
farhan <farhankhanmmb2004@gmail.com>
2025-02-27 20:48:13 +0530

    origin issue

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from 'cors';
 const app=express();
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
+    origin:'*',
     credentials:true
 }));
 app.use(express.json({limit:"16Kb"}));
