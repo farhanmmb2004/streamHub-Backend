93b323ac338245d32e4bf1ffd8a92c507d2a029d
farhan <farhankhanmmb2004@gmail.com>
2025-02-27 20:26:01 +0530

    fixing cors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from 'cors';
 const app=express();
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:process.env.CORS_ORIGIN,
     credentials:true
 }));
 app.use(express.json({limit:"16Kb"}));
