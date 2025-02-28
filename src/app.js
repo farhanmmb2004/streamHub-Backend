568510892c373830f09d482967f0b4541c2018e5
farhan <farhankhanmmb2004@gmail.com>
2025-02-28 17:51:08 +0530

    fixing cors

--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ import cookieParser from "cookie-parser";
 import cors from 'cors';
 const app=express();
 app.use(cors({
-    origin:'*',
+    origin:"https://stream-hub-frontend.vercel.app",
     credentials:true
 }));
+// http://localhost:5173
 app.use(express.json({limit:"16Kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static('public'));
